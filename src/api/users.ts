27be69245618885e5d778ba27231b755b8a66eca
CSKIM <rlackstjq987@gmail.com>
2024-03-01 17:48:16 +0900

    feat: add array update function for users

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,5 +1,13 @@
-import { postFirebase } from "./firebase";
+import { postFirebase, pushFirebaseArray } from "./firebase";
 
 export const postUser = (body: user) => {
   postFirebase<"users">("users", body);
+};
+
+export const pushUserPersonalSchedule = (id: string, value: string) => {
+  pushFirebaseArray<"users">("users", id, "personalSchedules", value);
+};
+
+export const pushUserGroup = (id: string, value: string) => {
+  pushFirebaseArray<"users">("users", id, "groups", value);
 };
