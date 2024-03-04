c376660b2bcec9814ec6347ce17940d5ff59497f
yeongjunekoh <rhdudwns0@naver.com>
2024-03-04 21:46:24 +0900

    chore: husky test

--- src/components/button/ExampleButton/ExampleButton.tsx
+++ src/components/button/ExampleButton/ExampleButton.tsx
@@ -26,13 +26,23 @@ interface ExampleButtonProps {
 /**
  * Primary UI component for user interaction
  */
-export const ExampleButton = ({ primary = false, size = "medium", backgroundColor, label, ...props }: ExampleButtonProps) => {
-
+//  test
+export const ExampleButton = ({
+  primary = false,
+  size = "medium",
+  backgroundColor,
+  label,
+  ...props
+}: ExampleButtonProps) => {
+  console.log(primary, size, backgroundColor);
+  
   return (
-    <button type="button"  {...props}>
+    <button type="button" {...props}>
       {label}
-      <h1 className="text-3xl font-bold underline"> {/*스토리북 실행 이후 해당 스타일이 적용되는 것을 확인 할 수 있습니다.*/}
-      Hello world!
+      <h1 className="text-3xl font-bold underline">
+        {" "}
+        {/*스토리북 실행 이후 해당 스타일이 적용되는 것을 확인 할 수 있습니다.*/}
+        Hello world!
       </h1>
     </button>
   );
