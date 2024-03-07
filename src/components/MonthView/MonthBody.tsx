c0adad5157f29cac0707d07e1fa51bd57abf5a04
CSKIM <rlackstjq987@gmail.com>
2024-03-08 04:55:39 +0900

    feat: add calendar generator

--- src/components/MonthView/MonthBody.tsx
+++ src/components/MonthView/MonthBody.tsx
@@ -1,21 +1,42 @@
+import { generateCalendar } from "@/lib/functions/calendar";
 import { useSearchParams } from "next/navigation";
 import React, { useState } from "react";
 
 function MonthBody() {
-  const [date, setDate] = useState<Date>();
+  const [date, setDate] = useState<Date>(new Date());
   const month = useSearchParams().get("month") as string;
   const year = useSearchParams().get("year") as string;
-  const now = new Date();
-
-  if (year !== undefined && month !== undefined && now === undefined) {
+  if (year !== null && month !== null) {
     const newData = new Date(parseInt(year), parseInt(month) - 1, 1);
-    setDate(newData);
-  } else if (now === undefined) {
-    const newData = new Date();
     setDate(newData);
   }
 
-  console.log(date);
+  function getWeekCount(date: Date) {
+    if (!date) throw new Error("날짜가 입력되지 않았습니다!");
+
+    const { firstDay, lastDay } = getFirstAndLastDay(date);
+    const lastDate = lastDay.getDate();
+    const firstDayOfWeek = firstDay.getDay();
+
+    if (lastDate === 28 && firstDayOfWeek === 0) return 4;
+    else if (lastDate === 31 && (firstDayOfWeek === 5 || firstDayOfWeek === 6)) return 6;
+    else if (lastDate === 30 && firstDayOfWeek === 6) return 6;
+    else return 5;
+  }
+  function getFirstAndLastDay(date: Date) {
+    const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    return { firstDay, lastDay };
+  }
+
+  function rangeGenerator() {
+    // if (date === undefined) throw new Error("Date setting Logic Error !");
+    const totalLine = getWeekCount(date);
+    const alpha = generateCalendar(date, totalLine);
+    console.log(alpha);
+    return 2;
+  }
+  console.log(rangeGenerator());
   return (
     <div>
       <ul>
