566a046d2e223cab93730a3df0d2677569dfef14
CSKIM <rlackstjq987@gmail.com>
2024-03-09 12:21:48 +0900

    feat: add monthView-header date logic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,17 +2,26 @@
 
 import React from "react";
 import ButtonIterator from "../ButtonIterator/ButtonIterator";
+import { useSearchParams } from "next/navigation";
 
 const Header = () => {
   const selectViewButton = ["month", "week", "day"];
-
+  let month = useSearchParams()?.get("month") as string;
+  let year = useSearchParams()?.get("year") as string;
+  if (!month || !year) {
+    const now = new Date();
+    year = String(now.getFullYear());
+    month = String(now.getMonth() + 1);
+  }
   return (
     <header className="flex flex-col justify-center items-center bg-yellow-200">
       <section>
         <ButtonIterator IterateData={selectViewButton} />
       </section>
       <section className="flex w-full">
-        <h1>2024 년 03월</h1>
+        <h1>
+          {year} 년 {month}월
+        </h1>
       </section>
     </header>
   );
