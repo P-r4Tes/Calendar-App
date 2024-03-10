1c7ea888013a74c9d6be325260edd93e26bb9068
entrolEC <euichan1108@gmail.com>
2024-03-10 18:10:12 +0900

    fix: pointer y position fix

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -10,12 +10,12 @@ type SidebarProps = {
 const Sidebar = ({ groups }: SidebarProps) => {
   const [selectedIdx, setSelectedIdx] = useState(0);
   const height = groups.length * 80 + 80;
-  const pointerY = 16 + selectedIdx * 64 + 28;
+  const pointerY = selectedIdx * 64 + 24 - 4;
 
   return (
     <div className="flex h-screen">
-      <aside className={"relative flex items-center w-20 bg-[#383A57] rounded-l-3xl"} style={{ height: height }}>
-        <div className="flex flex-col items-center space-y-4 p-4">
+      <aside className={"flex items-center w-20 bg-[#383A57] rounded-l-3xl"} style={{ height: height }}>
+        <div className="relative flex flex-col items-center space-y-4 p-4">
           {groups.map((group, idx) => {
             const onSelect: MouseEventHandler<HTMLAnchorElement> = () => {
               setSelectedIdx(idx);
