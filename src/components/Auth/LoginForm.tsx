23aff1a3ef870b6dbf4fde2ca42d293664807e7a
정세희 <zanne1218@naver.com>
2024-03-18 17:47:59 +0900

    chore: modify file structure

--- src/components/Auth/LoginForm.tsx
+++ src/components/Auth/LoginForm.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+"use client";
+import { useEffect, useState } from "react";
 import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "@/lib/firebaseAuth";
 import { doc, setDoc } from "firebase/firestore";
